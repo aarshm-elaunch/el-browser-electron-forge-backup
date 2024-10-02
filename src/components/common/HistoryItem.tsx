d821cc4f82965135a7cea365197a5fb9869d0dd2
sagar_k <sagar.k@elaunchinfotech.in>
2024-10-02 12:19:24 +0530

    create history and download UI

---
+++ src/components/common/HistoryItem.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Avatar, Box, Typography } from '@mui/material'
+import Image from '../../assets/images/history.jpg'
+
+const HistoryItem = () => {
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: '14px', py: '10px', px: '6px', "&:hover": { bgcolor: "#E4E4E4", borderRadius: '8px' } }}>
+            <Avatar
+                alt="Remy Sharp"
+                src={Image}
+                sx={{ width: 36, height: 36 }}
+            />
+            <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'start', justifyContent: "space-between" }}>
+                <Box>
+                    <Typography sx={{ color: (theme) => theme.palette.primary.dark, fontSize: 14, fontWeight: 400 }}>Free Images From Unspalsh</Typography>
+                    <Typography sx={{ color: (theme) => theme.palette.primary.dark, fontSize: 12, fontWeight: 300 }}>Unsplash.com</Typography>
+                </Box>
+                <Typography sx={{ color: (theme) => theme.palette.primary.dark, fontSize: 12, fontWeight: 400 }}>16 : 55 PM</Typography>
+            </Box>
+        </Box>
+    )
+}
+
+export default HistoryItem
